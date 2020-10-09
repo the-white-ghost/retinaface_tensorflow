2a0dfb48a925eab3a5c265c2cce85a8e235bf11d
unknown <glvikram99@gmail.com>
2020-10-10 02:14:33 +0530

    loss change

--- modules/losses.py
+++ modules/losses.py
@@ -43,32 +43,6 @@ def MultiBoxLoss(num_class=2, neg_pos_ratio=3):
         loss_class = tf.where(mask_neg,
                               1 - class_pred[:, 0][..., tf.newaxis], 0)
 
-        # 2. hard negative mining
-        loss_class = tf.reshape(loss_class, [num_batch, num_prior])
-        loss_class_idx = tf.argsort(loss_class, axis=1, direction='DESCENDING')
-        loss_class_idx_rank = tf.argsort(loss_class_idx, axis=1)
-        mask_pos_per_batch = tf.reshape(mask_pos, [num_batch, num_prior])
-        num_pos_per_batch = tf.reduce_sum(
-                tf.cast(mask_pos_per_batch, tf.float32), 1, keepdims=True)
-        num_pos_per_batch = tf.maximum(num_pos_per_batch, 1)
-        num_neg_per_batch = tf.minimum(neg_pos_ratio * num_pos_per_batch,
-                                       tf.cast(num_prior, tf.float32) - 1)
-        mask_hard_neg = tf.reshape(
-            tf.cast(loss_class_idx_rank, tf.float32) < num_neg_per_batch,
-            [num_batch * num_prior, 1])
-
-        # 3. classification loss including positive and negative examples
-        loss_class_mask = tf.logical_or(mask_pos, mask_hard_neg)
-        loss_class_mask_b = tf.broadcast_to(loss_class_mask,
-                                            tf.shape(class_pred))
-        filter_class_true = tf.boolean_mask(tf.cast(mask_pos, tf.float32),
-                                            loss_class_mask)
-        filter_class_pred = tf.boolean_mask(class_pred, loss_class_mask_b)
-        filter_class_pred = tf.reshape(filter_class_pred, [-1, num_class])
-        loss_class = tf.keras.losses.sparse_categorical_crossentropy(
-            y_true=filter_class_true, y_pred=filter_class_pred)
-        loss_class = tf.reduce_mean(loss_class)
-
         return loss_loc, loss_landm, loss_class
 
     return multi_box_loss
